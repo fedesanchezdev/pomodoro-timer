9bafbad7683f76414416962c26f9029cdf32ea75
Federico Sanchez <fede.dir@gmail.com>
2023-02-27 00:33:18 -0300

    pruebas

--- js/script.js
+++ js/script.js
@@ -80,7 +80,7 @@ function stopTimer() {
 function playAlarm() {
     player.innerHTML = `<audio id = "player2" autoplay controls loop playsinline muted><source src="./audio/alarm.mp3" type="audio/mpeg"></audio>`;
     credits.innerHTML = `<a href="https://www.youtube.com/@tonyannmusic/featured" target="_blank">"iPhone alarm as a piano ballad" by Tony Ann<br>subscribe to his channel</a>`;
-    toggleMute();
+    setTimeout(toggleMute, 1000);
 }
 
 function toggleMute() {
