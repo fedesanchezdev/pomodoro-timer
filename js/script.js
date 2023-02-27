e0362ab571e2cde2056e67c2f6181818708e29d2
Federico Sanchez <fede.dir@gmail.com>
2023-02-26 23:01:28 -0300

    player visible

--- js/script.js
+++ js/script.js
@@ -78,7 +78,7 @@ function stopTimer(){
 }
 
 function playAlarm(){
-    player.innerHTML = `<audio autoplay loop><source src="./audio/alarm.mp3" type="audio/mpeg"></audio>`;
+    player.innerHTML = `<audio autoplay controls loop><source src="./audio/alarm.mp3" type="audio/mpeg"></audio>`;
     credits.innerHTML = `<a href="https://www.youtube.com/@tonyannmusic/featured" target="_blank">"iPhone alarm as a piano ballad" by Tony Ann<br>subscribe to his channel</a>`;
 }
 
